ad3d5ddbd9cd8e0df10c12770f896ed11dbcb363
Weslley de Campos <weslleydrum@hotmail.com>
2022-08-20 20:15:55 -0300

    feat: ensure all encripted functions is working

--- src/site/MD5/Md5.java
+++ src/site/MD5/Md5.java
@@ -1,16 +1,99 @@
 package site.MD5;
 
 import site.Algorithm.Algorithm;
-
+import site.MD5.MD5Constants;
 public class Md5  implements Algorithm {
     byte[] input;
 
+    MD5Constants md5ConstantsImpl = null;
     public Md5(byte[] input) {
         this.input = input;
+        this.md5ConstantsImpl = new MD5Constants();
     }
     @Override
     public String encrypt() {
         int messageLength = this.input.length;
-        return "";
+        int blockNumber = ((messageLength + 8) >>> 6)  + 1;
+        int totalSize = blockNumber << 6;
+        int[] TABLE_T = this.md5ConstantsImpl.getK();
+
+        byte[] paddingBytes = new byte[totalSize - messageLength];
+
+        long messageLenBits = (long)messageLength << 3;
+
+        for(int i=0 ; i<8; i++){
+            paddingBytes[paddingBytes.length - 8 +i] = (byte)messageLength;
+            messageLenBits >>>=8;
+        }
+
+        int a = this.md5ConstantsImpl.INIT_A;
+        int b = this.md5ConstantsImpl.INIT_B;
+        int c = this.md5ConstantsImpl.INIT_C;
+        int d = this.md5ConstantsImpl.INIT_D;
+
+        int [] buffer = new int [16];
+        for(int i=0; i<blockNumber ; i++) {
+            int index = i << 6;
+            for(int j=0 ; j< 64; j++, index++) {
+                buffer[j >> 2] = ((int) ((index < messageLength) ? this.input[index] : paddingBytes[index - messageLength]) << 24) | (buffer[j >>> 2] >>> 8);
+            }
+                int originalA = a;
+                int originalB = b;
+                int originalC = c;
+                int originalD = d;
+
+                for(int j = 0 ; j<64; j++) {
+                    int div16 = j >>> 4;
+                    int f = 0;
+                    int bufferIndex = j;
+                    switch (div16) {
+                        case 0:
+                            f = (b & c) | (~b & d);
+                            break;
+                        case 1:
+                            f = (b & d ) | (~b & d);
+                            bufferIndex = (bufferIndex * 5 + 1) & 0x0F;
+                            break;
+                        case 2:
+                            f = b ^ c ^ d;
+                            bufferIndex = (bufferIndex*3 + 5) & 0x0F;
+                        case 3:
+                            f = c ^(b | ~d);
+                            bufferIndex = (bufferIndex* 7) & 0x0F;
+                    }
+                    int temp = b + Integer.rotateLeft(a + f + buffer[bufferIndex] +TABLE_T[j], this.md5ConstantsImpl.r[div16 << 2] | (j & 3));
+                    a = d;
+                    d = c;
+                    c = b;
+                    b= temp;
+                }
+                a += originalA;
+                b += originalB;
+                c += originalC;
+                d += originalD;
+
+        }
+        byte[] md5 = new byte[16];
+        int count = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            int n = (i == 0) ? a : ((i == 1) ? b : ((i == 2) ? c : d));
+            for (int j = 0; j < 4; j++)
+            {
+                md5[count++] = (byte)n;
+                n >>>= 8;
+            }
+        }
+        return "0x" + toHexString(md5);
+    }
+
+    private String toHexString(byte[] b)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < b.length; i++)
+        {
+            sb.append(String.format("%02X", b[i] & 0xFF));
+        }
+        return sb.toString();
     }
 }
--- src/site/Main.java
+++ src/site/Main.java
@@ -1,27 +1,36 @@
 package site;
 
 import site.SHA512.Sha512;
-
+import site.MD5.Md5;
 import java.nio.charset.StandardCharsets;
+
+import site.Utils.RandomString;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         String encrypted = "";
+        long start = 0;
+        long end = 0;
 
-        if(args.length != 2){
-            throw new Error("args must be equal 2");
+        if (args.length < 2) {
+            throw new Error("args must be have at last 2");
         }
 
-        if(args[0].equals("SHA512")) {
+        if (args[0].equals("SHA512")) {
+            start = System.currentTimeMillis();
             Sha512 algorithm = new Sha512(args[1].getBytes(StandardCharsets.UTF_8));
             encrypted = algorithm.encrypt();
+            end = System.currentTimeMillis();
         }
 
-        if(args[0].equals("MD5")) {
-            // implementar aqui
-            encrypted = "";
+        if (args[0].equals("MD5")) {
+            start = System.currentTimeMillis();
+            Md5 algorithm = new Md5(args[1].getBytes(StandardCharsets.UTF_8));
+            encrypted = algorithm.encrypt();
+            end = System.currentTimeMillis();
         }
 
-        System.out.print(encrypted + "\n");
+        System.out.println("Word " + args[1] +" encrypted in algorithm " + args[0]+ " is: " + encrypted + "\n");
+        System.out.println("Elapsed Time in milli seconds for execution of algorithm " + args[0] + " is: " + (end-start)+ "ms" + "\n");
     }
 }
